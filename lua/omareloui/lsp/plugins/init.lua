fa8f89eb1dcc373e5204c0c41e4dc8734e3c50c8
Omar Eloui <contact@omareloui.com>
2025-10-18 19:41:52 +0300

    chore(lint): remove auto linting

--- lua/omareloui/lsp/plugins/init.lua
+++ lua/omareloui/lsp/plugins/init.lua
@@ -276,12 +276,10 @@ return {
 
       lint.linters_by_ft = local_config.get("lsp.linters_by_ft", {})
 
-      local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
-
-      vim.api.nvim_create_autocmd(
-        { "BufEnter", "BufWritePost", "InsertLeave", "TextChanged" },
-        { group = lint_augroup, callback = try_lint }
-      )
+      -- vim.api.nvim_create_autocmd(
+      --   { "BufEnter", "BufWritePost", "InsertLeave", "TextChanged" },
+      --   { group = vim.api.nvim_create_augroup("lint", { clear = true }), callback = try_lint }
+      -- )
 
       local cspell_ns = lint.get_namespace "cspell"
 

a4ca9283fd4bf4af7aef0cf50b6386ef493990eb
Omar Eloui <contact@omareloui.com>
2026-06-23 18:22:12 +0300

    feat(folds): remeber folds for each file

--- lua/omareloui/config/init.lua
+++ lua/omareloui/config/init.lua
@@ -196,6 +196,36 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   end,
 })
 
+-- Save folds
+vim.api.nvim_create_autocmd({ "BufWinLeave", "BufWritePost", "WinLeave" }, {
+  group = vim.api.nvim_create_augroup("AutoSaveFolds", { clear = true }),
+  callback = function(args)
+    if vim.b[args.buf].view_activated then
+      vim.cmd.mkview { mods = { emsg_silent = true } }
+    end
+  end,
+})
+
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  group = vim.api.nvim_create_augroup("AutoLoadFolds", { clear = true }),
+  callback = function(args)
+    if not vim.b[args.buf].view_activated then
+      local filetype = vim.api.nvim_get_option_value("filetype", { buf = args.buf })
+      local buftype = vim.api.nvim_get_option_value("buftype", { buf = args.buf })
+      local ignore_filetypes = {
+        "gitcommit",
+        "gitrebase",
+        "svg",
+        "hgcommit",
+      }
+      if buftype == "" and filetype and filetype ~= "" and not vim.tbl_contains(ignore_filetypes, filetype) then
+        vim.b[args.buf].view_activated = true
+        vim.cmd.loadview { mods = { emsg_silent = true } }
+      end
+    end
+  end,
+})
+
 -- Automatically open the quickfix window after certain quickfix commands are executed.
 vim.api.nvim_create_autocmd("QuickFixCmdPost", {
   group = vim.api.nvim_create_augroup("AutoOpenQuickfix", { clear = true }),
